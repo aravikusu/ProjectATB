d234a2b64f2f38fb05531b299b561c9d78ec9384
Aravix <49846328+aravikusu@users.noreply.github.com>
2023-01-18 00:42:16 +0100

    Smoother TransitionCamera

--- autoload/TransitionCamera.gd
+++ autoload/TransitionCamera.gd
@@ -1,14 +1,22 @@
 extends Camera3D
 
+var tween: Tween
+
 func transition(from: Camera3D, to: Camera3D, duration: float = 1.0):
 	fov = from.fov
 	cull_mask = from.cull_mask
-	
-	global_transform = from.global_transform
+
 	
 	current = true
 	
-	var tween = create_tween().set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN_OUT)
+	if tween != null:
+		if tween.is_running():
+			tween.stop()
+		else:
+			global_transform = from.global_transform
+	else:
+		global_transform = from.global_transform
+	tween = create_tween().set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN_OUT)
 	tween.tween_property(self, "global_transform", to.global_transform, duration)
 	tween.tween_property(self, "fov", to.fov, duration)
 	
